750176d9ce0a7ae5b23b282767410655a0319ab0
krowflow <kuzznkrow@gmail.com>
2025-04-27 20:31:28 -0400

    Add basic WaveformHUD component for HUD waveform feature

---
+++ src/components/WaveformHUD.tsx
@@ -0,0 +1,19 @@
+// HUD Waveform Augment - Waveform HUD Component
+
+import React from 'react';
+
+/**
+ * WaveformHUD Component
+ * 
+ * A simple HUD (Heads-Up Display) component for the waveform visualization.
+ * This component will be expanded in future iterations.
+ */
+const WaveformHUD: React.FC = () => {
+  return (
+    <div className="waveform-hud">
+      Waveform HUD Active
+    </div>
+  );
+};
+
+export default WaveformHUD;
